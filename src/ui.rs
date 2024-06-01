5387fd5bc221650a1cdde56dd0c127e198d4b8f1
Stef Robbe <stef.robbe@protonmail.com>
2024-06-01 16:33:11 +0200

    feat: ui improvements

--- src/ui.rs
+++ src/ui.rs
@@ -1,6 +1,6 @@
 use ratatui::{
     layout::{Alignment, Constraint, Direction, Layout},
-    style::{Color, Style},
+    widgets::{Block, BorderType},
     Frame,
 };
 
@@ -8,20 +8,25 @@ use crate::app::App;
 
 /// Renders the user interface widgets.
 pub fn render(app: &mut App, frame: &mut Frame) {
-    let style: Style = Style::default().fg(Color::White).bg(Color::Black);
-
     let main_layout = Layout::default()
         .direction(Direction::Vertical)
         .constraints(vec![Constraint::Percentage(20), Constraint::Percentage(80)])
         .split(frame.size());
 
-    app.text_area.set_style(style);
-    // search_box.set_cursor_line_style(Style::default());
-    app.text_area.set_placeholder_text("Search...");
+    app.text_area
+        .set_placeholder_text("Start typing to search...");
+    app.text_area.set_block(
+        Block::bordered()
+            .border_type(BorderType::Rounded)
+            .title("rgtui")
+            .title_alignment(Alignment::Center),
+    );
 
     // TODO: fix this
     let res_box = app.search_res.clone();
-    let res_box = res_box.alignment(Alignment::Left).style(style);
+    let res_box = res_box
+        .alignment(Alignment::Left)
+        .block(Block::bordered().border_type(BorderType::Rounded));
 
     frame.render_widget(app.text_area.widget(), main_layout[0]);
     frame.render_widget(res_box, main_layout[1]);
